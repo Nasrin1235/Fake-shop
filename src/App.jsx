76552571f013820b2f8e354f18c99f10ba467426
Liang_Wang <liangwang.fr@gmail.com>
2024-10-28 13:53:20 +0100

    add Cart component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Homepage from "./Pages/Homepage";
 import ProductsPage from "./Pages/ProductsPage";
 import DetailsPages from "./Pages/DetailsPages";
+import Cart from "./Pages/Cart";
 import CheckoutPage from "./Pages/CheckoutPage";
 import NotFound from "./Pages/NotFound";
 import { ProductsProvider } from "./contex/ProductsContext";
@@ -13,6 +14,7 @@ function App() {
       <Route path="/" element={< Homepage/>} />
       <Route path="/products" element={<ProductsPage />} />
       <Route path="/products/:id" element={<DetailsPages />} />
+      <Route path="/cart" element={<Cart />} />
       <Route path="/checkout" element={<CheckoutPage />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
---
+++ src/Pages/Cart.jsx
@@ -0,0 +1,7 @@
+function Cart() {
+  return (
+    <div>DetailsPages</div>
+  )
+}
+
+export default Cart
