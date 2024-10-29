b639c737af0e5cc48622b286440318e98bcd0b86
Liang_Wang <liangwang.fr@gmail.com>
2024-10-29 16:32:51 +0100

    modesetting

--- src/Pages/Layout.jsx
+++ src/Pages/Layout.jsx
@@ -6,13 +6,19 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import "./Header.css";
 import "./Footer.css";
+import { useContext } from 'react'
+import { ProductContext } from '../contex/ProductsContext'
 
 const Layout = ({ children }) => {
+  const {mode, modeSwitch} = useContext(ProductContext)
+  const showMode =  mode === 'lightMode' ? '' : 'dark'
+
   return (
     <div>
       <header className="header">
         <a className="shop-name">Shop</a>
         <div className="header-icons">
+          <button data-role="mode" onClick={modeSwitch} className={showMode}></button>
           <a>
             <FontAwesomeIcon icon={faUser} className="icon" />
           </a>
@@ -25,7 +31,7 @@ const Layout = ({ children }) => {
         </div>
       </header>
 
-      <main>{children}</main>
+      <main className={showMode}>{children}</main>
 
       <footer className="footer">
         <p>© {new Date().getFullYear()} Fake Shop. All rights reserved.</p>
