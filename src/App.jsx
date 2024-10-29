e13ab511a59a1223566c2bb3c567004db4b9ebc9
Liang_Wang <liangwang.fr@gmail.com>
2024-10-29 15:20:59 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -8,24 +8,27 @@ import NotFound from "./Pages/NotFound";
 import { useContext } from 'react'
 import { ProductContext } from './contex/ProductsContext'
 import './app.css'
+import Layout from "./Pages/Layout";
 
 
 function App() {
-  const {mode, modeSwitch} = useContext(ProductContext)
+  const { mode, modeSwitch } = useContext(ProductContext)
   const modeClass = mode === 'lightMode' ? 'app' : 'app dark'
 
   return (
-    <div className={modeClass}>
-      <button onClick={modeSwitch} data-role="mode">{mode}</button>
-      <Routes>
-        <Route path="/" element={< Homepage />} />
-        <Route path="/products" element={<ProductsPage />} />
-        <Route path="/products/:id" element={<DetailsPages />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/checkout" element={<CheckoutPage />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </div>
+      <div className={modeClass}>
+        <button onClick={modeSwitch} data-role="mode">{mode}</button>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/products" element={<ProductsPage />} />
+            <Route path="/products/:id" element={<DetailsPages />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/checkout" element={<CheckoutPage />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Layout>
+      </div>
   );
 }
 
