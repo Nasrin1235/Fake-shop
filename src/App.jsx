e04cf319497e92038cc8b8097c048f43240ea488
Nasrin1235 <nasrin.mahmoudi@gmx.de>
2024-10-29 14:55:59 +0100

    changeApp

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,19 @@ import NotFound from "./Pages/NotFound";
 import { ProductsProvider } from "./contex/ProductsContext";
 import Layout from "./Pages/Layout";
 
-
 function App() {
   return (
     <ProductsProvider>
-      <Layout >
-    <Routes>
-      <Route path="/" element={< Homepage/>} />
-      <Route path="/products" element={<ProductsPage />} />
-      <Route path="/products/:id" element={<DetailsPages />} />
-      <Route path="/cart" element={<Cart />} />
-      <Route path="/checkout" element={<CheckoutPage />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-    </Layout>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/products" element={<ProductsPage />} />
+          <Route path="/products/:id" element={<DetailsPages />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<CheckoutPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Layout>
     </ProductsProvider>
   );
 }
