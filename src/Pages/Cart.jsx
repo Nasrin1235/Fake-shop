b78dd37593148fae61cd63466ec05f700ace35ce
Liang_Wang <liangwang.fr@gmail.com>
2024-10-29 10:31:27 +0100

    update

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -5,7 +5,7 @@ import { NavLink } from 'react-router-dom'
 import './cart.css'
 
 function Cart() {
-  const { AddToCard, clearCart, totalPrice, totolItems, productsIncart} = useContext(ProductContext)
+  const { AddToCard, clearCart, totalPrice, totalItems, productsIncart } = useContext(ProductContext)
   const randomId = Math.floor(Math.random() * 20)
 
 
@@ -14,34 +14,31 @@ function Cart() {
   }, [productsIncart])
 
   return (
-    <div className='cart'>
-      <button onClick={() => AddToCard(randomId)}>add one to cart</button><br />
-      <button onClick={clearCart}>clear cart</button>
-      <h1>Shopping Basket</h1>
-      <ul>
-        {productsIncart.length > 0
-          ? productsIncart.map(product => (
-            <li key={product.id}>
-              <Product
-                title={product.title}
-                imageSrc={product.image}
-                info={product.description}
-                price={product.price}
-                qty={product.quantity}
+    <div className='cartPage'>
+      <section>
+        <button onClick={() => AddToCard(randomId)}>add one to cart</button><br />
+        <button onClick={clearCart}>clear cart</button>
+        <h1>Shopping Basket</h1>
+        <ul>
+          {productsIncart.length > 0
+            ? productsIncart.map(product => (
+              <li key={product.id}>
+                <Product 
                 id={product.id}
-              />
-            </li>
-          ))
-          : <p>Your basket is empty.</p>
-        }
-      </ul>
-      <div className='total'>
-        <button data-role="clear">Clear Basket</button>
-        <div>
-          <span>{`Total ${totolItems} items: ${totalPrice} €`}</span>
-          <NavLink to="/checkout">Proceed to Checkout</NavLink>
+                />
+              </li>
+            ))
+            : <p>Your basket is empty.</p>
+          }
+        </ul>
+        <div className='total'>
+          <button data-role="clear">Clear Basket</button>
+          <div>
+            <span>{`Total ${totalItems} items: ${totalPrice} €`}</span>
+            <NavLink to="/checkout">Proceed to Checkout</NavLink>
+          </div>
         </div>
-      </div>
+      </section>
     </div>
   );
 }
