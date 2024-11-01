c7e572869721b533951482bc703e0fc7a85a74d1
Liang_Wang <liangwang.fr@gmail.com>
2024-11-01 12:50:34 +0100

    update

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -5,7 +5,7 @@ import { ProductOfCaterory } from '../components/ProductOfCaterory'
 import { useParams } from 'react-router-dom';
 
 export default function Category() {
-  const { categories, products, calculatePriceRange, lowestPrice, highestPrice } = useContext(ProductContext);
+  const { categories, products, calculatePriceRange, lowestPrice, highestPrice, givenLowestPrice, givenHighestPrice} = useContext(ProductContext);
 
   const { category } = useParams()
   let filteredProducts
@@ -16,11 +16,14 @@ export default function Category() {
     }
   }, [category, products, categories]);
 
-  console.log("lowestPrice", lowestPrice)
-  console.log("highestPrice", highestPrice)
+  console.log("givenLowestPrice", givenLowestPrice)
+  console.log("givenHighestPrice", givenHighestPrice)
 
+  const minPrice = givenLowestPrice || lowestPrice;
+  const maxPrice = givenHighestPrice || highestPrice
+  
   if (category === 'all') {
-    filteredProducts = products.filter(product => product.price >= lowestPrice && product.price <= highestPrice);
+    filteredProducts = products.filter(product => product.price >= minPrice && product.price <= maxPrice);
     if (filteredProducts.length > 0)
       return (
         <ul className='productsInCategory'>
@@ -37,7 +40,7 @@ export default function Category() {
     const productsInCategory = products.filter(
       product => product.category === category
     )
-    filteredProducts = productsInCategory.filter(product => product.price >= lowestPrice && product.price <= highestPrice);
+    filteredProducts = productsInCategory.filter(product => product.price >= minPrice && product.price <= maxPrice);
 
     if (filteredProducts.length > 0)
       return (
