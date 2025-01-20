3fd7e5807f23fe4de8fefc682482aee44ad32e04
Nasrin1235 <nasrin.mahmoudi@gmx.de>
2025-01-20 11:56:31 +0100

    user

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,24 @@
+
 import express from "express";
 import path from "path";
 import { Product } from "./src/models/Product.js";
 import { fileURLToPath } from "url";
 import { dbConnection } from "./src/scripts/dbConnection.js";
 import { seed } from "./src/scripts/seed.js";
-import cors from 'cors'
-
+import cors from 'cors';
+import userRouter from "./src/routes/userRouter.js";
 const app = express();
-const PORT = process.env.PORT || 3001;
-
+const PORT = process.env.PORT || 3005;
 const _fileName = fileURLToPath(import.meta.url);
 const _path = path.dirname(_fileName);
 const frontendDistPath = path.join(_path, "../frontend/dist");
 
-// Initialize DB connection when the server starts
 await dbConnection();
-
 app.use(express.static(frontendDistPath));
-app.use(cors())
+app.use(cors());
+app.use(express.json());
+app.use("/api", userRouter);
 
-// Check if products exist and seed the DB if necessary
 const initializeProducts = async () => {
   try {
     const products = await Product.find();
@@ -32,15 +31,12 @@ const initializeProducts = async () => {
   }
 };
 
-// Initialize products on server startup
 initializeProducts();
 
-// Return frontend on any undefined route
 app.get("*", (req, res) => {
   res.sendFile(path.join(frontendDistPath, "index.html"));
 });
 
-// Listen on the specified port
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
